70c449b267b33887a27c81e320c5d3e3426b0dc6
Kaciade <valerypolyakov60@gmail.com>
2023-02-19 22:19:14 +0400

    Задание 1 выполнено

---
+++ Tasks/Task_01/Program.cs
@@ -0,0 +1,37 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите левую часть массива: ");
+int min = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите правую часть массива: ");
+int max = int.Parse(Console.ReadLine() ?? "0");
+int [,] matrix = new int [m, n];
+Random random = new Random();
+
+Console.WriteLine($"\nВаш массив {m} x {n}:");
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = random.Next(min, max);
+        Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+for (int i = 0; i < matrix.GetLength(1); i++)
+{
+    int sum = 0;
+    int average = 0;
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        sum = sum + matrix[j, i];
+    }
+    average = sum / m;
+    Console.WriteLine($"Среднее арифметическое {i+1} столбца = {average}");
+}
+Console.WriteLine();
